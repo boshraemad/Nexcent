d280c83112c60ea5e6cfc449ad5b5682c6d26424
Boshra-Emad47 <112584666+boshraemad@users.noreply.github.com>
2024-08-25 23:48:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector(".burger").onclick=()=>{
+    if(document.querySelector(".toggle-menu").style.display==="none"){
+        document.querySelector(".toggle-menu").style.display="block";
+        document.body.style.overflow="hidden";
+    }else{
+        document.querySelector(".toggle-menu").style.display="none";
+        document.body.style.overflow="visible";
+    }
+}
